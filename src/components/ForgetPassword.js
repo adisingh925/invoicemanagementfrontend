8679b16240eedc03473860d63f53c36f1ff783d1
adisingh925 <adisingh925@gmail.com>
2024-04-15 22:43:15 +0530

    na

--- src/components/ForgetPassword.js
+++ src/components/ForgetPassword.js
@@ -17,6 +17,7 @@ function ForgetPassword() {
     await sendResetLink(email.email, localStorage.getItem("token"));
   };
 
+  
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
