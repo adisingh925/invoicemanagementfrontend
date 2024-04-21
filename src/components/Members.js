a056e7ee6f86dd0619f5950335e0d82aacf832f0
adisingh925 <adisingh925@gmail.com>
2024-04-22 01:23:41 +0530

    na

--- src/components/Members.js
+++ src/components/Members.js
@@ -407,12 +407,12 @@ function Members() {
                       </label>
                       <select
                         id="membership"
-                        name="member_membership_type"
+                        name="membership"
                         onChange={onChange}
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                         required
                       >
-                        <option value="" disabled selected>
+                        <option defaultValue="" disabled selected>
                           Select membership type
                         </option>
                         {membershipData.map((membershipData) => (
